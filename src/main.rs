b6a94bfbbdb2f2abfc108df621f702571a9443a2
James Walker <james.chunho@gmail.com>
2024-07-14 22:23:27 +0800

    basic benchmark and test script

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,32 @@
-use std::fs;
+use std::{fs, ops::Div};
+
+use serialize::serialize_to_string;
 
 mod parser;
 mod serialize;
 
 fn main() {
-    let path = r"D:\Audio Projects (Reaper)\temp3.rpp";
+    let path = r"D:\Audio Projects (Reaper)\New\unwelcome school 2\unwelcome school 2_5.RPP";
     let rpp = fs::read_to_string(&path).unwrap();
-    println!("Loaded to string, parsing...");
-    let rpp = parser::parse_element(&rpp).unwrap();
 
-    println!("Success!");
+    println!("Loaded to string, parsing");
+    let element = parser::parse_element(&rpp).unwrap();
+
+    println!("Serialializing to result.rpp");
+    let text = serialize_to_string(&element);
+    fs::write("result.rpp", text).unwrap();
+
+    println!("Benchmark loop");
+    let loops = 30;
+    let now = std::time::Instant::now();
+    for _ in 0..loops {
+        serialize_to_string(&element);
+    }
+    let elapsed = now.elapsed();
+    println!(
+        "Did {} loops. Total: {:?}, Average: {:?}",
+        loops,
+        elapsed,
+        elapsed.div(loops)
+    );
 }
